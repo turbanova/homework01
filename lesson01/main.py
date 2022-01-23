6506d85b6ed0e7f445ae58bc8f7135787dff3d07
Anna Turbanova <orange.velvet@yandex.ru>
2022-01-23 21:53:42 +0300

    Lesson 01_01

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,2 +1,5 @@
-x = input("Enter variable:")
-print("Variable: " + x)
+x = int(input("Enter your ages"))
+r = input("Enter Your Name")
+print("Hello " + r + "! You are looking good for " + str(x))
+
+
